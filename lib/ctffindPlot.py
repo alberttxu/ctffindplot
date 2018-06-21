b27ebe25f798e4bd3dcc34d57d4e139e4df18a61
Albert Xu <albert.t.xu@gmail.com>
2018-06-21 19:13:57 +0000

    multiple plots in one png

--- lib/ctffindPlot.py
+++ lib/ctffindPlot.py
@@ -6,12 +6,7 @@ import PyGnuplot as gp
 
 ctffindOutputTxt = sys.argv[1]
 logfile = 'log.txt'
-defocusPlot = 'defocus.png'
-phasePlot = 'phase.png'
-aziAstigPlot = 'azimuthAstig.png'
-xcorrPlot = 'crossCorr.png'
-resolutionFitPlot = 'angstromFit.png'
-
+outputPlot = 'plot.png'
 
 def parseCtffindOutput(outputTxt):
     try:
@@ -24,8 +19,6 @@ def parseCtffindOutput(outputTxt):
     # expected filename format: *_1234_ali.mrc
     filename = lines[1].split()[3]
     picNumber = int(filename.split('_')[-2])
-    #labels = lines[4][10:].split(';')
-    #labels = [col[6:].strip() for col in labels]
     values = [float(x) for x in lines[5].split()][1:]
     # convert phase shift to degrees
     values[3] = float('%.6f' % (values[3] * 180/3.14))
@@ -42,13 +35,14 @@ def readLog(log):
         return np.array([[]])
 
 def updateLog(log, outputTxt, debug=False):
+    # read data and sort
     data = readLog(log).tolist()
     if data[0] == []: # handle empty log
         data.pop(0)
     data.append(parseCtffindOutput(outputTxt))
     data = [list(x) for x in set(tuple(x) for x in data)]
     data.sort(key=lambda x: x[0])
-
+    # write back
     with open(log, 'w') as f:
         for picNumber, *values in data:
             f.write(' '.join((1+len(values)) * ['{}'])
@@ -57,24 +51,31 @@ def updateLog(log, outputTxt, debug=False):
         subprocess.call("cat %s" % log, shell=True)
     return data
 
-def savePlot(outputPng, log, *columns):
-    gp.c("set terminal pngcairo")
-    gp.c("set output '%s'" % outputPng)
+def savePlot(log, *columns, title='', ylabel=''):
     gp.c("set key off")
     gp.c("set xtics 1")
+    if title:
+        gp.c("set title '%s'" % title)
+    if ylabel:
+        gp.c("set ylabel '%s'" % ylabel)
+
     if len(columns) == 1:
         gp.c("plot '{}' u 1:{} w lp".format(log, columns[0]))
     elif len(columns) == 2:
         gp.c("plot '{0}' u 1:{1} w lp, '{0}' u 1:{2} w lp"
              .format(log, *columns))
 
+
 if __name__ == "__main__":
-    #updateLog(logfile, ctffindOutputTxt)
-    #print(parseCtffindOutput(ctffindOutputTxt))
     updateLog(logfile, ctffindOutputTxt)
-    savePlot(defocusPlot, logfile, 2, 3)
-    savePlot(aziAstigPlot, logfile, 4)
-    savePlot(phasePlot, logfile, 5)
-    savePlot(xcorrPlot, logfile, 6)
-    savePlot(resolutionFitPlot, logfile, 7)
+    gp.c("set terminal pngcairo dashed enhanced size 1200,800")
+    gp.c("set output '%s'" % outputPlot)
+    gp.c("set multiplot layout 2,3")
+    gp.c("set tmargin 2")
+    savePlot(logfile, 2, 3, title='Defocus 1 and 2')
+    savePlot(logfile, 4, title='Azimuth of Astigmatism')
+    savePlot(logfile, 5, title='Phase Shift, Degrees')
+    savePlot(logfile, 6, title='Cross Correlation')
+    savePlot(logfile, 7, title='Resolution of Fit, Å')
+    gp.c("unset multiplot")
 
