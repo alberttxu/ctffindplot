7512c77e6f3509eaafa9008ff485b6a0a17f86bf
Albert Xu <albert.t.xu@gmail.com>
2019-06-06 02:16:30 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 import setuptools
 
 setuptools.setup(name='ctffindPlot',
-           version='0.0.1',
-           description='Sequentially plot ctffind results from aligned micrographs',
-           url='https://github.com/alberttxu/ctffindPlot',
-           author='Albert Xu',
-           author_email='albert.t.xu@gmail.com',
-           license='MIT',
-           packages=setuptools.find_packages(),
-           install_requires=[
-               'PyGnuplot',
-           ],
-)
+        version='0.0.1',
+        description='Sequentially plot ctffind results from aligned micrographs',
+        url='https://github.com/alberttxu/ctffindPlot',
+        author='Albert Xu',
+        author_email='albert.t.xu@gmail.com',
+        license='MIT',
+        packages=setuptools.find_packages(),
+        install_requires=[
+            'PyGnuplot',
+            ],
+        entry_points={
+            'console_scripts': [
+                'ctffindPlot = ctffindPlot.__main__:main'
+                ]
+            }
+        )
