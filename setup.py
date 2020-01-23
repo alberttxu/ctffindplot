3e5580f5eaf6f52b416a17063863d1d958859634
Albert Xu <albert.t.xu@gmail.com>
2020-01-23 16:35:32 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="ctffindplot",
-    version="0.0.5",
+    version="0.0.6",
     description="Sequentially plot ctffind results from aligned micrographs",
     url="https://github.com/alberttxu/ctffindplot",
     author="Albert Xu",
